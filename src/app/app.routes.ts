ad48b89b9c2efe6d97e2b0e27dec16102118f504
HariK-developer <hariswdeveloper@gmail.com>
2024-07-26 07:46:10 +0530

    not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 import { WishListComponent } from './wish-list/wish-list.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 export const routes: Routes = [
 
@@ -21,5 +22,13 @@ export const routes: Routes = [
     {
         path: 'wishlist',
         component: WishListComponent,
+    },
+    {
+        path: '404',
+        component: NotfoundComponent,
+    },
+    {
+        path: '**',
+        redirectTo: '404',
     }
 ];
